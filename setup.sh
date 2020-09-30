30dcc88c1fea80c1f4e3340f9f4ae7a797b2fc23
Axel Goblet <axel.goblet@bigdatarepublic.nl>
2020-09-30 17:38:17 +0200

    updated setup with helm operator

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ set -e
 # cluster
 minikube start --cni=calico --memory=10000 --cpus=4 --network-plugin=cni
 
-## flux
+# flux
 sudo snap install fluxctl --classic
 kubectl create ns flux
 export GHUSER="axelgobletbdr"
@@ -15,3 +15,12 @@ fluxctl install \
   --git-url=git@github.com:${GHUSER}/flux-get-started \
   --git-path=namespaces,workloads \
   --namespace=flux | kubectl apply -f -
+# add the following args to flux deployment:
+# --sync-garbage-collection
+# --manifest-generation=true
+
+# helm operator
+kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/1.2.0/deploy/crds.yaml
+kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/1.2.0/deploy/rbac.yaml
+kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/1.2.0/deploy/deployment.yaml
+kubectl patch -n flux deploy helm-operator -p='{"spec": {"template":{"spec":{"containers":[{"name": "helm-operator", "args": ["--enabled-helm-versions=v3"]}]}}}}'
