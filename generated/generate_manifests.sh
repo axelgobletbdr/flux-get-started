ceaade26e8b50169c70db4cde8a4a49cb181d8dc
Axel Goblet <axel.goblet@bigdatarepublic.nl>
2020-09-30 15:37:27 +0200

    added another deployment to generated

--- generated/generate_manifests.sh
+++ generated/generate_manifests.sh
@@ -4,3 +4,4 @@ set -e
 
 kubectl create --dry-run=client -o yaml ns nginx
 kubectl create --dry-run=client -o yaml deploy --image nginx -n nginx nginx
+kubectl create --dry-run=client -o yaml deploy --image nginx -n nginx nginx2
