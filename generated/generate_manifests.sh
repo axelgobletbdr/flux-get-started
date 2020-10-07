46c70e467734e36273d9273b61c31f0e55f2f437
Axel Goblet <axel.goblet@bigdatarepublic.nl>
2020-10-07 10:53:24 +0200

    added nginx3

--- generated/generate_manifests.sh
+++ generated/generate_manifests.sh
@@ -2,6 +2,9 @@
 
 set -e
 
-kubectl create --dry-run -o yaml ns nginx
 echo ---
 kubectl create --dry-run -o yaml deploy --image nginx -n nginx nginx2
+echo ---
+kubectl create --dry-run -o yaml deploy --image nginx -n nginx nginx3
+echo ---
+kubectl create --dry-run -o yaml ns nginx
