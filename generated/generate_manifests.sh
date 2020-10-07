c881ddf09fa6fc10e871db2a9642b7b757e0a027
Axel Goblet <axel.goblet@bigdatarepublic.nl>
2020-10-07 10:31:49 +0200

    added second generated nginx

--- generated/generate_manifests.sh
+++ generated/generate_manifests.sh
@@ -5,3 +5,4 @@ set -e
 kubectl create --dry-run -o yaml ns nginx
 echo ---
 kubectl create --dry-run -o yaml deploy --image nginx -n nginx nginx
+kubectl create --dry-run -o yaml deploy --image nginx -n nginx nginx2
